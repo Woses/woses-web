4fcd54988044e1459c252aefcf47ee72fa5fa578
rune <42575387+rrune@users.noreply.github.com>
2022-01-23 21:29:09 +0100

    spotify++

--- public/js/components/spotify.js
+++ public/js/components/spotify.js
@@ -3,8 +3,8 @@ import ListElement from "./listElement.js";
 const api = {
   url: "https://api.woses.de/spotifyRecent",
   getData() {
-    return m.request({url: this.url})
-  }
+    return m.request({ url: this.url });
+  },
 };
 
 const Spotify = {
@@ -12,38 +12,54 @@ const Spotify = {
   apiData: undefined,
   error: false,
 
-  async oninit({attrs ,state}) {
-    state.data = attrs.data
+  async oninit({ attrs, state }) {
+    state.data = attrs.data;
     try {
-      state.apiData = (await api.getData()).Data
+      state.apiData = (await api.getData()).Data;
+      state.data.link = state.apiData[0].url;
     } catch (error) {
       console.log("Spotify: ", error);
-      state.error = true
+      state.error = true;
     }
 
     setInterval(async () => {
       try {
-        state.apiData = (await api.getData()).Data
+        state.apiData = (await api.getData()).Data;
+        state.data.link = state.apiData[0].url;
       } catch (error) {
         console.log("Spotify: ", error);
-        state.error = true
+        state.error = true;
       }
-    }, 60000)
+    }, 60000);
   },
-  view({state}) {
+  view({ state }) {
     return m(
       ListElement,
       { data: state.data },
-      m("div.text", [
-        state.apiData ? [
-          state.apiData[0]["@attr"].nowplaying ? 
-            m("p", "Hört gerade:")
-           :m("p", "Zuletzt gehört:"),
-          m("p", `${state.apiData[0].artist["#text"]} - ${state.apiData[0].name}`),
-        ] : state.error ? "Fehler" : "Lädt..."
-      ])
+      state.apiData
+        ? [
+            m("div.text",
+            [
+              m("div", [
+                state.apiData[0]["@attr"].nowplaying
+                  ? m("p", "Hört gerade:")
+                  : m("p", "Zuletzt gehört:"),
+                m(
+                  "p",
+                  `${state.apiData[0].artist["#text"]} - ${state.apiData[0].name}`
+                ),
+              ]),
+            ]),
+            m("img.spotify", {
+              src: state.apiData[0].image[2]["#text"],
+              alt: state.apiData[0].image[2]["#text"],
+            }),
+          ]
+        : state.error
+        ? m("div.text", "Fehler")
+        : m("div.text", "Lädt...")
     );
-  }
-}
+  },
+};
 
 export default Spotify;
